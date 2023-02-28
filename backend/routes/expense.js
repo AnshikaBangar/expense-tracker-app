be30af770abb94a05d50a50e0969baaf2cbfe6d3
AnshikaBangar <69668387+AnshikaBangar@users.noreply.github.com>
2023-02-28 13:47:06 +0530

    Update expense.js

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -3,10 +3,15 @@ const express = require("express");
 const routes = express.Router();
 
 const manageExpense = require("../controllers/expenses");
+const userAuth = require("../middleware/auth");
 
 routes.post("/add-expense", manageExpense.addExpense);
 
-routes.get("/get-expense", manageExpense.getExpense);
+routes.get(
+  "/get-expense",
+  userAuth.userAuthontication,
+  manageExpense.getExpense
+);
 
 routes.delete("/delete-expense/:id", manageExpense.deleteExpense);
 
