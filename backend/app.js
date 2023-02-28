f91b373d6b297c4466ad43806b4f80f1cd7b8130
AnshikaBangar <69668387+AnshikaBangar@users.noreply.github.com>
2023-02-28 12:33:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,15 @@ const cors = require("cors");
 
 const signup = require("./routes/user");
 const sequelize = require("./utils/database");
+const expenseDetail=require("./routes/expense");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/user", signup);
+
+app.use("/expense",expenseDetail)
 
 sequelize
   .sync()
